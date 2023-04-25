5816ed8aa1ccb88ba2eeb8ccfdf52e7b5a91e947
Leila Zrim <leilazrim@gmail.com>
2023-04-25 08:49:16 +0200

    wrong assertion changed - corrected

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,17 +5,17 @@ suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
-      assert.fail(
+      assert.isNull(
         null,
         'This is an optional error description - e.g. null is null'
       );
-      assert.fail(1, '1 is not null');
+      assert.isNotNull(1, '1 is not null');
     });
     // #2
     test('#isDefined, #isUndefined', function () {
-      assert.isNull(null, 'null is not undefined');
-      assert.isNotNull(undefined, 'undefined IS undefined');
-      assert.isNotNull('hello', 'A string is not undefined');
+      assert.fail(null, 'null is not undefined');
+      assert.fail(undefined, 'undefined IS undefined');
+      assert.fail('hello', 'A string is not undefined');
     });
     // #3
     test('#isOk, #isNotOk', function () {
